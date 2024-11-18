43f9dd1438911ef7bdcd119b3cf8e1d250284d08
Moses Alcantara <infinitech.mosesalcantara@gmail.com>
2024-11-18 11:26:53 +0800

    dropdowns

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,72 +1,75 @@
+"use client";
+
 import React from "react";
 import Link from "next/link";
+import {
+  Dropdown,
+  DropdownTrigger,
+  DropdownMenu,
+  DropdownItem,
+  Button,
+} from "@nextui-org/react";
 
 const Navbar = () => {
   return (
     <>
-      <div className="flex gap-3 bg-sky-600 p-5">
+      <div className="flex gap-3 items-center bg-sky-600 p-5">
         <Link href="/">
           <h3 className="text-white font-semibold">Dashboard</h3>
         </Link>
 
-        <Link href="/transactions">
-          <h3 className="text-white font-semibold">Transaction Monitoring</h3>
-        </Link>
+        <Dropdown>
+          <DropdownTrigger>
+            <Button
+              disableRipple
+              className="p-0 bg-transparent data-[hover=true]:bg-transparent"
+              variant="light"
+            >
+              <h3 className="text-white font-semibold text-base">
+                Transaction Monitoring
+              </h3>
+            </Button>
+          </DropdownTrigger>
 
-        <Link href="/contracts">
-          <h3 className="text-white font-semibold">Contract Monitoring</h3>
-        </Link>
+          <DropdownMenu className="text-center bg-transparent">
+            <DropdownItem>
+              <Link href="/accounts">
+                <h3 className="font-semibold">Accounts</h3>
+              </Link>
+            </DropdownItem>
+            <DropdownItem>
+              <Link href="/transactions">
+                <h3 className="font-semibold">Transactions</h3>
+              </Link>
+            </DropdownItem>
+          </DropdownMenu>
+        </Dropdown>
 
-        {/* 
-          <div className="dropdown">
-            <div
-              tabIndex={0}
-              role="button"
-              className="text-white font-semibold"
+        <Dropdown>
+          <DropdownTrigger>
+            <Button
+              disableRipple
+              className="p-0 bg-transparent data-[hover=true]:bg-transparent"
+              variant="light"
             >
-              Transaction Monitoring
-            </div>
-            <ul
-              tabIndex={0}
-              className="menu dropdown-content bg-base-100 rounded-box z-[1] p-2"
-            >
-              <li>
-                <Link href="/transactions">
-                  <h3 className="font-semibold">Transactions</h3>
-                </Link>
-              </li>
-              <li>
-                <Link href="/accounts">
-                  <h3 className="font-semibold">Accounts</h3>
-                </Link>
-              </li>
-            </ul>
-          </div>
-
-          <div className="dropdown">
-            <div
-              tabIndex={0}
-              role="button"
-              className="text-white font-semibold"
-            >
-              Contract Monitoring
-            </div>
-            <ul
-              tabIndex={0}
-              className="menu dropdown-content bg-base-100 rounded-box z-[1] p-2"
-            >
-              <li>
-                <Link href="/contracts">
-                  <h3 className="font-semibold">Contracts</h3>
-                </Link>
-              </li>
-              <li>
-                <Link href="/clients">
-                  <h3 className="font-semibold">Clients</h3>
-                </Link>
-              </li>
-            </ul>
-          </div> */}
+              <h3 className="text-white font-semibold text-base">
+                Contract Monitoring
+              </h3>
+            </Button>
+          </DropdownTrigger>
+          <DropdownMenu className="text-center">
+            <DropdownItem>
+              <Link href="/accounts">
+                <h3 className="font-semibold">Clients</h3>
+              </Link>
+            </DropdownItem>
+            <DropdownItem>
+              <Link href="/contracts">
+                <h3 className="font-semibold">Contracts</h3>
+              </Link>
+            </DropdownItem>
+          </DropdownMenu>
+        </Dropdown>
       </div>
     </>
   );
