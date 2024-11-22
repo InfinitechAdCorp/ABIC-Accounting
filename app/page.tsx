c12e197fe0933dc37e81e581dfbbc0ed05b5d517
Infinitech-MosesAlcantara <infinitech.mosesalcantara@gmail.com>
2024-11-22 13:30:12 +0800

    populate charts with data

--- app/page.tsx
+++ app/page.tsx
@@ -23,51 +23,53 @@ const Dashboard = async () => {
 
             <div className="grid grid-cols-4 gap-3 mb-3">
               <Card>
-                <CardBody className="text-center">
-                  <h1 className="font-semibold text-base">Accounts</h1>
+                <CardBody className="text-center px-20 py-10">
+                  <h1 className="font-semibold text-xl">Accounts</h1>
                   <h1 className="font-bold text-2xl">{counts.accounts}</h1>
                 </CardBody>
               </Card>
 
               <Card>
-                <CardBody className="text-center">
-                  <h1 className="font-semibold text-base">Transactions</h1>
+                <CardBody className="text-center px-20 py-10">
+                  <h1 className="font-semibold text-xl">Transactions</h1>
                   <h1 className="font-bold text-2xl">{counts.transactions}</h1>
                 </CardBody>
               </Card>
 
               <Card>
-                <CardBody className="text-center">
-                  <h1 className="font-semibold text-base">Clients</h1>
+                <CardBody className="text-center px-20 py-10">
+                  <h1 className="font-semibold text-xl">Clients</h1>
                   <h1 className="font-bold text-2xl">{counts.clients}</h1>
                 </CardBody>
               </Card>
 
               <Card>
-                <CardBody className="text-center">
-                  <h1 className="font-semibold text-base">Contracts</h1>
+                <CardBody className="text-center px-20 py-10">
+                  <h1 className="font-semibold text-xl">Contracts</h1>
                   <h1 className="font-bold text-2xl">{counts.contracts}</h1>
                 </CardBody>
               </Card>
             </div>
 
-            <Card className="mb-3 h-52">
-              <CardBody className="text-center">
-                <Barchart
-                  title="Transactions Per Account"
-                  data={charts.accountsWithTransactions}
-                />
-              </CardBody>
-            </Card>
+            <div className="grid grid-cols-2 gap-3">
+              <Card className="mb-3 h-72">
+                <CardBody className="text-center">
+                  <Barchart
+                    title="Transactions Per Account"
+                    data={charts.accountsWithTransactions}
+                  />
+                </CardBody>
+              </Card>
 
-            <Card className="mb-3 h-52">
-              <CardBody className="text-center">
-                <Barchart
-                  title="Contracts Per Client"
-                  data={charts.clientsWithContracts}
-                />
-              </CardBody>
-            </Card>
+              <Card className="mb-3 h-72">
+                <CardBody className="text-center">
+                  <Barchart
+                    title="Contracts Per Client"
+                    data={charts.clientsWithContracts}
+                  />
+                </CardBody>
+              </Card>
+            </div>
           </CardBody>
         </Card>
       </div>
