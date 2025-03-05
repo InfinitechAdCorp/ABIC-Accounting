c834ce1a9c24ce64b8edc7a5f58939414bedc298
Infinitech-MosesAlcantara <infinitech.mosesalcantara@gmail.com>
2025-03-05 09:30:44 +0800

    middleware

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,8 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
+import { logout } from "@/components/globals/auth";
 
-export function middleware(request: NextRequest) {
+export async function middleware(request: NextRequest) {
   const routes = [
     "/accounts",
     "/dashboard",
@@ -10,13 +11,23 @@ export function middleware(request: NextRequest) {
     "/listings",
     "/tools",
   ];
-  
-  const isLoggedIn = request.cookies.get("isLoggedIn");
 
-  if (routes.includes(request.nextUrl.pathname)) {
+  const isLoggedIn = request.cookies.get("isLoggedIn");
+  const pathname = request.nextUrl.pathname;
+
+  if (pathname == "/") {
+    await logout();
+    return NextResponse.next();
+  } else if (routes.includes(pathname)) {
     if (isLoggedIn) {
       return NextResponse.next();
     }
-    return NextResponse.redirect(new URL("/", request.url));
   }
+  return NextResponse.redirect(new URL("/", request.url));
 }
+
+export const config = {
+  matcher: [
+    "/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)",
+  ],
+};
